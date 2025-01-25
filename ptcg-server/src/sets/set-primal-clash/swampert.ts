aaaec8734cef4b7a9bd37fff1a00cfebea6d7378
Ryan Bown <rbown1993@hotmail.com>
2025-01-24 19:03:58 -0500

    getting growth to work

---
+++ ptcg-server/src/sets/set-primal-clash/swampert.ts
@@ -0,0 +1,145 @@
+import { StoreLike, State,  GameMessage, ChoosePokemonPrompt,   PlayerType, SlotType, PowerType    } from '../../game';
+import { PokemonCard } from '../../game/store/card/pokemon-card';
+import { GameError } from '../../game/game-error';
+import { BoardEffect, CardType, Stage } from '../../game/store/card/card-types';
+import { Card } from '../../game/store/card/card';
+import {  PutDamageEffect } from '../../game/store/effects/attack-effects';
+import { PowerEffect } from '../../game/store/effects/game-effects';
+import { Effect } from '../../game/store/effects/effect';
+import { AttackEffect } from '../../game/store/effects/game-effects';
+import { CheckProvidedEnergyEffect } from '../../game/store/effects/check-effects';
+import { ChooseCardsPrompt } from '../../game/store/prompts/choose-cards-prompt';
+import { CardList } from '../../game/store/state/card-list';
+import { ShuffleDeckPrompt } from '../../game/store/prompts/shuffle-prompt';
+
+function* useDivingSearch(next: Function, store: StoreLike, state: State,
+  self: Swampert, effect: PowerEffect): IterableIterator<State> {
+  const player = effect.player;
+  let cards: Card[] = [];
+
+  if (player.deck.cards.length === 0) {
+    throw new GameError(GameMessage.CANNOT_USE_POWER);
+  }
+
+  const deckTop = new CardList();
+
+  yield store.prompt(state, new ChooseCardsPrompt(
+    player,
+    GameMessage.CHOOSE_CARDS_TO_PUT_ON_TOP_OF_THE_DECK,
+    player.deck,
+    {},
+    { min: 1, max: 1, allowCancel: false }
+  ), selected => {
+    cards = selected || [];
+    next();
+  });
+
+  player.deck.moveCardsTo(cards, deckTop);
+
+  player.forEachPokemon(PlayerType.BOTTOM_PLAYER, cardList => {
+    if (cardList.getPokemonCard() === self) {
+      cardList.addBoardEffect(BoardEffect.ABILITY_USED);
+    }
+  });
+
+  return store.prompt(state, new ShuffleDeckPrompt(player.id), order => {
+    player.deck.applyOrder(order);
+    if (order === null) {
+      return state;
+    }
+    deckTop.applyOrder(order);
+    deckTop.moveToTopOfDestination(player.deck);
+  });
+}
+
+
+
+
+
+
+
+
+export class Swampert extends PokemonCard {
+  public stage: Stage = Stage.STAGE_2;
+  public cardType: CardType = CardType.WATER;
+  public hp: number = 140;
+  public weakness = [{ type: CardType.GRASS }];
+  public retreat = [CardType.COLORLESS, CardType.COLORLESS, CardType.COLORLESS];
+
+  public powers = [{
+    name: 'Growth',
+    powerType: PowerType.ANCIENT_TRAIT,
+    barrage: true,
+    text: 'When you attach an Energy  card from your hand to this Pokémon (except with an attack, Ability, or Trainer card), you may attach 2 Energy cards.'
+  },
+{
+    name: 'Diving Search',
+    powerType: PowerType.ABILITY,
+    text: 'Once during your turn (before your attack), you may search your deck for a card. Shuffle your deck, then put that card on top of it.'
+  }
+];
+  
+  public attacks = [
+  {
+    name: 'Hydro Pump',
+    cost: [CardType.FIRE, CardType.COLORLESS, CardType.COLORLESS],
+    damage: 40,
+    text: 'This attack does 30 more damage for each {W} Energy attached to this Pokémon.'
+  }];
+
+  public set = 'PRC';
+  public cardImage: string = 'assets/cardback.png';
+  public setNumber: string = '36';
+  public name = 'Swampert';
+  public fullName = 'Swampert PRC';
+
+  public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
+
+
+    
+
+    if (effect instanceof PowerEffect && effect.power === this.powers[1]) {
+        const generator = useDivingSearch(() => generator.next(), store, state, this, effect);
+        return generator.next().value;
+  
+      }
+    
+
+      if (effect instanceof AttackEffect && effect.attack === this.attacks[0]) {
+        const player = effect.player;
+        
+        return store.prompt(state, new ChoosePokemonPrompt(
+          player.id,
+          GameMessage.CHOOSE_POKEMON_TO_DAMAGE,
+          PlayerType.TOP_PLAYER,
+          [SlotType.ACTIVE, SlotType.BENCH],
+          { min: 1, max: 1, allowCancel: false }
+        ), selected => {
+          const targets = selected || [];
+  
+          const checkProvidedEnergyEffect = new CheckProvidedEnergyEffect(player, player.active);
+          store.reduceEffect(state, checkProvidedEnergyEffect);
+  
+          let energyCount = 0;
+          checkProvidedEnergyEffect.energyMap.forEach(em => {
+            energyCount += em.provides.filter(cardType =>
+              cardType === CardType.WATER || cardType === CardType.ANY
+            ).length;
+          });
+  
+          const damage = energyCount * 30;
+  
+          targets.forEach(target => {
+            const damageEffect = new PutDamageEffect(effect, damage);
+            damageEffect.target = target;
+            store.reduceEffect(state, damageEffect);
+          });
+  
+          return state;
+        });
+      }
+      
+      return state
+  }
+
+}
